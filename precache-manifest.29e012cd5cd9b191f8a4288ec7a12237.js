c92a192ba25ee62bfbc67144f1c139e167253dfa
riskinputra <riski.n.putra@gmail.com>
2020-08-04 17:30:27 +0700

    change template v1.0

---
+++ precache-manifest.29e012cd5cd9b191f8a4288ec7a12237.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b715226e1e39fbfad7f1e9bb6b4014a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e2c16a19f637f8f4969",
+    "url": "/static/css/main.cae08ad0.chunk.css"
+  },
+  {
+    "revision": "a52df7c1fa01c972ee36",
+    "url": "/static/js/2.9edaaa13.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9edaaa13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e2c16a19f637f8f4969",
+    "url": "/static/js/main.25a7ef5a.chunk.js"
+  },
+  {
+    "revision": "efe25b4b8d1f7af9da4b",
+    "url": "/static/js/runtime-main.6b3488d1.js"
+  },
+  {
+    "revision": "90b60f757be06dd37fd4142a9da8defd",
+    "url": "/static/media/author.90b60f75.jpg"
+  }
+]);
